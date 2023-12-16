ea7ae9e4e72740a0d9808debbd5584cc5c3454fd
swapsatoshi <swapsatoshi@gmail.com>
2023-12-16 20:51:56 +0800

    add token balance service function

---
+++ src/service/token.js
@@ -0,0 +1,10 @@
+import { serviceGET } from "./node";
+
+export const getTokenBalances = async (param, serviceUrl) => {
+  const searchParams = new URLSearchParams(param);
+  const route = await serviceGET(
+    `/api/v3/token/balances?${searchParams.toString()}`,
+    serviceUrl
+  );
+  return route;
+};
