14f8aa65eef7afa161548988f4ad2c590b6b528d
swapsatoshi <swapsatoshi@gmail.com>
2024-01-24 13:49:58 +0800

    refactor css theme folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import ThemeProvider from './context/ThemeProvider';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-tooltip/dist/react-tooltip.css';
 import 'react-json-view-lite/dist/index.css';
+import './theme';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+
+import ThemeProvider from './context/ThemeProvider';
 import ScrollToTop from './utils/route/scrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
